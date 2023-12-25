5a5a98938c0b31da6306a4f583178ed93cb15104
ElisabethGrothe <154772690+ElisabethGrothe@users.noreply.github.com>
2023-12-25 10:41:37 +0700

    Add files via upload

---
+++ art_guesser_script.js
@@ -0,0 +1,48 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    const nextButton = document.getElementById('next');
+    nextButton.addEventListener('click', loadNewArtwork);
+});
+
+function loadNewArtwork() {
+    // Example API call to fetch artwork data
+    // Replace with actual API call and handle response data
+    const artworkData = {
+        src: 'https://via.placeholder.com/600x400',
+        time: ['1800s', '1900s', '2000s', '2100s'],
+        period: ['Renaissance', 'Baroque', 'Modern', 'Contemporary'],
+        artist: ['Artist A', 'Artist B', 'Artist C', 'Artist D'],
+        location: ['France', 'Italy', 'USA', 'Japan'],
+        material: ['Oil', 'Acrylic', 'Watercolor', 'Digital']
+    };
+
+    updateArtwork(artworkData);
+}
+
+function updateArtwork(data) {
+    document.getElementById('artwork').src = data.src;
+    updateQuestion('time-question', 'When was this created?', data.time);
+    updateQuestion('period-question', 'What is the period?', data.period);
+    updateQuestion('artist-question', 'Who is the artist?', data.artist);
+    updateQuestion('location-question', 'Where was this created?', data.location);
+    updateQuestion('material-question', 'What material is used?', data.material);
+}
+
+function updateQuestion(containerId, questionText, options) {
+    const container = document.getElementById(containerId);
+    container.innerHTML = `<h3>${questionText}</h3>`;
+
+    options.forEach(option => {
+        const button = document.createElement('button');
+        button.textContent = option;
+        button.className = 'choice-button';
+        button.onclick = function() { handleChoice(option); };
+        container.appendChild(button);
+    });
+}
+
+function handleChoice(choice) {
+    // Placeholder for handling choices
+    // Implement logic to verify choice and update score
+    alert(`You chose: ${choice}`);
+}
